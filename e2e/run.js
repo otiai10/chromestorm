f9b1cc07c417fe9b6a7e5d779cf79947e44a9f30
Hiromu OCHIAI <otiai10@gmail.com>
2022-12-19 18:52:39 +0900

    Use macos-12 for E2E

--- e2e/run.js
+++ e2e/run.js
@@ -1,24 +1,29 @@
 const puppeteer = require("puppeteer");
 const path = require("path");
 
+// See https://pptr.dev/guides/chrome-extensions/
+
 (async () => {
   const ext = path.join(__dirname, "app");
   console.log(ext);
   const browser = await puppeteer.launch({
-    headless: "chrome",
+    headless: false,
     args: [
       `--disable-extensions-except=${ext}`,
       `--load-extension=${ext}`,
     ],
   });
   console.log(browser);
+  const page = await browser.newPage();
   const background = await browser.waitForTarget(
-    target => {
+    async target => {
       console.log(target.type());
+      console.log(await target.worker());
       return target.type() === 'page'
     }
   );
-  const page = await background.page();
+  // const page = await background.page();
   console.log(await page.title());
+  await page.close();
   await browser.close();
 })();
