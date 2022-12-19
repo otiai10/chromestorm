c49d90e1c77ecbd22f860215a0e489a6518efddb
Hiromu OCHIAI <otiai10@gmail.com>
2022-12-19 17:34:12 +0900

    Add `save` and `create`

--- __tests__/all-test.ts
+++ __tests__/all-test.ts
@@ -1,5 +1,9 @@
 import { Model } from "../src";
 
+// TODO: jest-chrome is not following the latest @types/chrome.
+//       It requires @types/chrome@0.0.193.
+import { chrome } from "jest-chrome";
+
 describe("new", () => {
     it("constructs new instance of model without saving", () => {
         class Player extends Model {
@@ -9,5 +13,33 @@ describe("new", () => {
         expect(otiai10).toBeInstanceOf(Player);
         expect(otiai10.name).toBe("otiai10");
         expect(otiai10.__id).toBe(null);
+    });
+});
+
+describe("save", () => {
+    it("saves the instance to your storage area", async () => {
+        class Player extends Model {
+            static override __area__ = chrome.storage.local;
+            public name: string;
+        }
+        const otiai10 = Player.new({ name: "otiai10" });
+        expect(otiai10).toBeInstanceOf(Player);
+        expect(otiai10.name).toBe("otiai10");
+        expect(otiai10.__id).toBe(null);
+        await otiai10.save();
+        expect(otiai10.__id).not.toBe(null);
+    });
+});
+
+describe("create", () => {
+    it("is just a shorthand of new().save()", async () => {
+        class Player extends Model {
+            static override __area__ = chrome.storage.local;
+            public name: string;
+        }
+        const otiai20 = await Player.create({ name: "otiai20" });
+        expect(otiai20).toBeInstanceOf(Player);
+        expect(otiai20.name).toBe("otiai20");
+        expect(otiai20.__id).not.toBe(null);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,39 @@ type ModelConstructor<T> = { new(props?: any): T }
 
 export class Model {
 
-    static new<T>(this: ModelConstructor<T>, props?: any): T {
+    static __namespace__?: string;
+    private static __name__<T>(this: ModelConstructor<T>): string {
+        return this["__namespace__"] ?? this.name;
+    }
+    static __nextID__: (dict?: { [key: string]: any }) => string = () => String(Date.now());
+    static __area__: chrome.storage.StorageArea;
+
+    static new<T>(this: ModelConstructor<T>, props?: Record<string, any>): T {
         const instance: T = new this(props);
-        Object.entries<any>(props).map(([key, value]) => {
+        instance["__id"] = null;
+        Object.entries<any>(props || {}).map(([key, value]) => {
             instance[key] = value;
         });
-        instance["__id"] = null;
         return instance;
     }
 
-    public __id: string | number | null;
+    static async create<T>(this: ModelConstructor<T>, props?: Record<string, any>): Promise<T> {
+        return await this["new"](props).save();
+    }
+
+    public __id: string | null;
+
+    async save<T>(this: T & Model): Promise<T> {
+        const namespace: string = this.constructor["__name__"]();
+        const area: chrome.storage.StorageArea = this.constructor["__area__"];
+        const ensemble = await area.get(namespace);
+        const dict = ((ensemble || {})[namespace] || {});
+        if (!this.__id) {
+            this.__id = this.constructor["__nextID__"](dict);
+        }
+        dict[this.__id!] = this;
+        await area.set({ [namespace]: dict });
+        return this;
+    }
+
 }
